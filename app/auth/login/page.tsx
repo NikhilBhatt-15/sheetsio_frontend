5775a789ec57b5f05d419e91e7588705dbfa5715
NikhilBhatt-15 <nikhilbhatt6980@gmail.com>
2025-03-09 03:02:06 +0530

    xxx

--- app/auth/login/page.tsx
+++ app/auth/login/page.tsx
@@ -1,12 +1,12 @@
 "use client";
 import Login from '@/app/components/LoginComponent';
 import { useRouter } from "next/navigation";
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { toast, Toaster } from 'sonner';
 
 const Page = () => {
     const router = useRouter();
-    const [loading, setLoading] = useState(false);
+    // const [loading, setLoading] = useState(false);
     // useEffect(() => {
     //     fetch(`${process.env.NEXT_PUBLIC_BACKEND_URL}/api/v1/auth/verify`, {
     //         method: 'GET',
@@ -47,9 +47,7 @@ const Page = () => {
         
             <div className='relative h-screen'>
                 <div className='flex justify-center items-center h-full bg-gradient-to-r from-gray-300 via-gray-500 to-gray-700'>
-                    {loading ? (<h1 className='text-4xl font-bold text-white'> 
-                    Loading...
-                    </h1>):(<Login onSubmit={onSubmit} />)}
+                    <Login onSubmit={onSubmit} />
                 </div>
                 <Toaster />
             </div>
